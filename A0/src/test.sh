c83eea9a7f150d212aec4b4845eb45276ce558f5
Daniel Grundtvig <Danielblyme@gmail.com>
2017-09-10 15:07:58 +0200

    Reordered some files, removed the testversion of file (file2() and added additional test files to test.sh

--- A0/src/test.sh
+++ A0/src/test.sh
@@ -5,21 +5,23 @@ set -e
 
 make
 
-
 echo "Generating a test_files directory.."
 mkdir -p test_files
 rm -f test_files/*
 
 
 echo "Generating test files.."
-printf "Hello, World!\n" > test_files/ascii.input
-printf "Hello, World!" > test_files/ascii2.input
-printf "Hello,\x00World!\n" > test_files/data.input
-printf "" > test_files/empty.input
-### TODO: Generate more test files ###
-
-
+printf "Hello, World!\n" > test_files/ascii.input ## ascii file
+printf "Hello, World!" > test_files/ascii2.input ## another ascii file, with nl char
+printf "Hello,\x00World!\n" > test_files/data.input ## file containing a char outside our ascii-text definition
+printf "" > test_files/empty.input	## empty file
+printf "Secret file" > test_files/secret.input && chmod -r test_files/secret.input ## make secret file
+printf "scale=4000; 4*a(1)" | bc -l > test_files/large_ascii.input ## generates a file containing pi with an amount of digits equal to the "scale" value (ascii)
+printf "scale=4000; 4*a(1)" | bc -l > test_files/large_ascii.input  && printf "\372 ">> test_files/large_non_ascii.input ## adds the "u acute" character to the end of a large ascii file
 echo "Running the tests.."
+
+
+
 exitcode=0
 for f in test_files/*.input
 do
@@ -35,4 +37,8 @@ do
     echo ">>> Success :-)"
   fi
 done
+
+
+chmod +r test_files/secret.input ## make secret file not secret again, so we are able to commit file in git 
+
 exit $exitcode
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CompSys-Assignments
 Assignments for Computer Systems
+
+Made by Daniel Grundtvig (btg689) and Emil Soerensen 
